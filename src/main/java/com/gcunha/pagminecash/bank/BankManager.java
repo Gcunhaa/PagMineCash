bb47b8642cde0b20ad20cdae6a2f32c483d97575
Gabriel <gsalomaoc@gmail.com>
2020-01-21 17:11:25 -0300

    Sistema SQL funcionando perfeitamente

--- src/main/java/com/gcunha/pagminecash/bank/BankManager.java
+++ src/main/java/com/gcunha/pagminecash/bank/BankManager.java
@@ -4,6 +4,8 @@ import com.gcunha.pagminecash.PagMineCash;
 import com.gcunha.pagminecash.cache.CacheOfflineCash;
 import com.gcunha.pagminecash.data.CashData;
 import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.libs.jline.internal.Nullable;
+import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 import java.util.UUID;
@@ -38,14 +40,19 @@ public class BankManager {
 
             bank = cacheOffline.get(uuid);
 
-            if(Bukkit.getOnlinePlayers().contains(uuid)) {
+            if(Bukkit.getOnlinePlayers().contains(uuid) && !this.cacheOnline.containsKey(uuid)) {
                 this.cacheOnline.put(uuid,bank);
                 this.cacheOffline.remove(uuid);
             }
 
         }
 
-        //TODO:VERIFICAR BANCO DE DADOS E RETORNAR NULO SE NAO TIVER
+        //VERIFICA BANCO DE DADOS E RETORNAR NULO SE NAO TIVER
+        bank = dataManager.getBank(uuid);
+
+        //Se jogador estiver offline adiciona no cacheOffline
+        if(Bukkit.getPlayer(uuid) == null && bank != null ) cacheOffline.add(bank);
+
         return bank;
     }
 
@@ -57,19 +64,37 @@ public class BankManager {
     public Bank createBank(UUID uuid){
         //Funcao que cria o banco
 
-        try{
-            System.out.print(dataManager.getBank(uuid).toString());
-        } catch (Exception e) {
-            e.printStackTrace();
+        Bank bank = null;
+
+        //Verifica se o jogador esta no cacheOffline
+        if(this.cacheOffline.contains(uuid)){
+            bank = this.cacheOffline.get(uuid);
+        }else{
+            bank = this.dataManager.getBank(uuid);
         }
-        //if(dataManager.getBank(uuid) == null){
-            //TODO:CRIA NO BANCO DE DADOS
+
+        if(bank == null){
+            this.dataManager.createBank(uuid);
+            bank = new Bank(uuid);
+        }
+
+        //Verifica se o jogador esta online ou nao, posteriormente adiciona ao chache de acordo com o estado dele
+        if(Bukkit.getPlayer(uuid) != null) {
+
+            if(this.cacheOffline.contains(uuid)){
+                this.cacheOffline.remove(uuid);
+            }
+
+            this.cacheOnline.put(uuid, bank);
+        }else{
+            this.cacheOffline.add(bank);
+        }
 
 
-        if(Bukkit.getOnlinePlayers().contains(uuid)) this.cacheOnline.put(uuid, null);
-
-
-        return null;
+        return bank;
     }
 
+    public HashMap<UUID, Bank> getCacheOnline() {
+        return cacheOnline;
+    }
 }
